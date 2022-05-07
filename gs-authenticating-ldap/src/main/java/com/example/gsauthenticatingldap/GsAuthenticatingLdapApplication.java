918af46ae9696f18926dfe5ddd19beb9d94821ca
kai993 <huf0618@icloud.com>
2022-05-07 12:32:48 +0900

    LDAPでユーザー認証

---
+++ gs-authenticating-ldap/src/main/java/com/example/gsauthenticatingldap/GsAuthenticatingLdapApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsauthenticatingldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAuthenticatingLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsAuthenticatingLdapApplication.class, args);
+	}
+
+}
---
+++ restservice/src/main/java/springboot/restservice/Greeting.java
@@ -0,0 +1,16 @@
+package springboot.com.example.restservice;
+
+import lombok.Getter;
+
+public class Greeting {
+    @Getter
+    private final long id;
+
+    @Getter
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id      = id;
+        this.content = content;
+    }
+}
